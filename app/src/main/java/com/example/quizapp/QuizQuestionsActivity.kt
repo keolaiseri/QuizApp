5718371186c003e8db1fd68bbfb62961582098ae
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2021-08-12 07:27:04 -1000

    implementing selected border

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -1,14 +1,23 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition : Int = 1
+    private var mQuestionsList : ArrayList<Question>? = null
+    private var mSelectedOptionPosition : Int = 0
+
 
     val progressBar = findViewById<ProgressBar>(R.id.progressBar)
     val tv_progress = findViewById<TextView>(R.id.tv_progress)
@@ -23,16 +32,24 @@ class QuizQuestionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionsList = Constants.getQuestions()
-        Log.i( "Questions Size", "${questionsList.size }")
+        mQuestionsList = Constants.getQuestions()
+        setQuestion()
 
+    }
 
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
+    private fun setQuestion(){
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition - 1]
 
+        defaultOptionsView()
 
-        progressBar.progress = currentPosition
-        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_three.setOnClickListener(this)
+        tv_option_four.setOnClickListener(this)
+
+        progressBar.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition" + "/" + progressBar.max
 
         tv_question.text = question!!.quesion
         iv_image.setImageResource(question.image)
@@ -44,7 +61,61 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
     }
 
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        options.add(0, tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_three)
+        options.add(3,tv_option_four)
 
+
+        for(option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    override fun onClick(v: View?)
+    {
+        when(v?.id)
+        {
+            R.id.tv_option_one ->
+            {
+                selectedOptionView(tv_option_one, 1)
+            }
+            R.id.tv_option_two ->
+            {
+                selectedOptionView(tv_option_two, 2)
+            }
+            R.id.tv_option_three ->
+            {
+                selectedOptionView(tv_option_three, 3)
+            }
+            R.id.tv_option_four ->
+            {
+                selectedOptionView(tv_option_four, 4)
+            }
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView,
+                                   selectedOptionNum: Int)
+    {
+     defaultOptionsView()
+     mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+
+    }
 
 
 }
